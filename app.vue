5a62c74037db695361c86a5b406fd2fc8c0eb1d2
Han Kyungsoo <dripcoke24@gmail.com>
2023-06-28 01:27:42 +0900

    メイン変更

--- app.vue
+++ app.vue
@@ -1,13 +1,6 @@
-<script setup lang="ts">
-const version = 2 + 1;
-</script>
 <template>
-  <div class="hello">Hello Nuxt {{ version }}!</div>
+  <div id="app">
+    <h1>Hello!</h1>
+    <p>This is message...</p>
+  </div>
 </template>
-<style>
-.hello {
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 3rem;
-  padding: 2rem;
-}
-</style>
